4d4995e3499a5d5b37310e1a5cb31c71b58f096a
AlexandraJuly <catmon0201@gmail.com>
2023-04-26 12:08:51 +0300

    1

---
+++ hw_38-main/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MainLayout from "./MainLayout/MainLayout";
+import todoData from "./todoData/todoData";
+
+function App() {
+  if (!(localStorage.getItem('todoData'))) {
+    localStorage.setItem('todoData', JSON.stringify(todoData))
+    console.log('else')
+  }
+
+  return (
+      < MainLayout />
+  );
+}
+
+export default App;
